7be6a27b14fc8715209ce89f04fadb59ac7297cd
Noel De Martin <noel@noeldemartin.com>
2024-07-19 18:27:20 +0200

    Configure PWA

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-full w-full flex-col bg-blue-50">
+    <div class="flex min-h-full w-full flex-col bg-sky-50">
         <main class="flex flex-1 flex-col items-center justify-center p-4">
             <h1 class="font-titillium text-center text-7xl">
                 Freedom Calculator
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,46 @@
 import { URL, fileURLToPath } from 'node:url';
 
-import components from 'unplugin-vue-components/vite';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import TailwindCSS from '@tailwindcss/vite';
+import Vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [vue(), tailwindcss(), components({ dts: false })],
+    plugins: [
+        Vue(),
+        TailwindCSS(),
+        Components({ dts: false }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Freedom Calculator',
+                short_name: 'Freedom Calculator',
+                description: 'What\'s your runway?',
+                theme_color: '#bae6fd',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            workbox: {
+                additionalManifestEntries: [
+                    '/apple-touch-icon.png',
+                    '/favicon-32x32.png',
+                    '/favicon-16x16.png',
+                    '/safari-pinned-tab.svg',
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
