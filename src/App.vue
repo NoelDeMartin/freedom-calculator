cac1f348716b3fe832950bc7e2f02605e05bb2b6
Noel De Martin <noel@noeldemartin.com>
2024-07-19 18:32:23 +0200

    Tweak mobile styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex min-h-full w-full flex-col bg-sky-50">
         <main class="flex flex-1 flex-col items-center justify-center p-4">
-            <h1 class="font-titillium text-center text-7xl">
+            <h1 class="font-titillium text-center text-5xl md:text-7xl">
                 Freedom Calculator
             </h1>
-            <p class="mt-2 text-center text-2xl font-light italic text-gray-700">
+            <p class="mt-2 text-center text-lg font-light italic text-gray-700 md:text-2xl">
                 What's your runway? If your income drops to 0 right now, how long will you last?
             </p>
-            <AppCalculator class="mt-8" />
+            <AppCalculator class="mt-6 md:mt-8" />
         </main>
 
         <AppFooter />
